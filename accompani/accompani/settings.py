ef4350f091e1fe9697224ababb5dea02b4ebcb82
Ankit Agarwal <ankit1990@gmail.com>
2015-08-20 23:20:23 -0700

    Adding accompani_calendar as installed app for accompani project

--- accompani/accompani/settings.py
+++ accompani/accompani/settings.py
@@ -30,6 +30,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = (
+    'accompani_calendar',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
