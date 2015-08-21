fcda73272e679ab08fa989afe63aadca4ba754f8
Ankit Agarwal <ankit1990@gmail.com>
2015-08-20 23:20:54 -0700

    1. Basic version which renders using a template

--- accompani/accompani_calendar/views.py
+++ accompani/accompani_calendar/views.py
@@ -2,5 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Index page of the accompani calendar app.
+
+
 def index(request):
-    return HttpResponse("Hello world!")
+
+    return render(request, "accompani_calendar/index.html")
