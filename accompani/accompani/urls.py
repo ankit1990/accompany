047ab92f9f046197f3b169bb9706f8c571ed3284
Ankit Agarwal <ankit1990@gmail.com>
2015-08-21 22:48:38 -0700

    Removing url mappings for admin app since I don't require it.

--- accompani/accompani/urls.py
+++ accompani/accompani/urls.py
@@ -1,7 +1,5 @@
 from django.conf.urls import patterns, include, url
-from django.contrib import admin
 
 urlpatterns = patterns('',
                        url(r'^accompani/calendar/', include('accompani_calendar.urls')),
-                       url(r'^accompani/admin/', include(admin.site.urls)),
                        )
