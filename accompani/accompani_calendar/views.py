e8fd2a14a1a4eb9483e5379d7c63f716209e455f
Ankit Agarwal <ankit1990@gmail.com>
2015-08-23 05:12:05 -0700

    Handle invalid_grant exception; session was storing a stale value of 'short-lived' access token which needs to be refreshed.

--- accompani/accompani_calendar/views.py
+++ accompani/accompani_calendar/views.py
@@ -4,6 +4,7 @@ from django.shortcuts import render
 import httplib2
 from django.http import HttpResponseRedirect
 from oauth2client.client import flow_from_clientsecrets
+from oauth2client.client import AccessTokenRefreshError
 from django.core.urlresolvers import reverse_lazy
 
 from apiclient.discovery import build
@@ -33,17 +34,32 @@ def index(request):
         credential = None
 
     if credential is None or credential.invalid:
-        authorize_url = FLOW.step1_get_authorize_url()
-        return HttpResponseRedirect(authorize_url)
+        return do_auth()
     else:
-        http = httplib2.Http()
-        http = credential.authorize(http)
+        try:
+            http = httplib2.Http()
+            http = credential.authorize(http)
 
-        # TODO(ankit): Make request here.
-        # service = build("plus", "v1", http=http)
+            # TODO(ankit): Make request here.
+            service = build("calendar", "v3", http=http)
+            eventResults = service.events().list(calendarId='primary', singleEvents=True).execute()
+            print eventResults.get('items', [])
 
+        except AccessTokenRefreshError:
+            return do_auth()
         return render(request, "accompani_calendar/index.html")
 
+
+# Fetches a list of calendar events for a given month and year.
+# Why is this API necessary? We show the 'month view' of the calendar; we just want to show all the events
+# which occur in the given month.
+def fetch_list(request, month, year):
+
+    pass
+
+def do_auth():
+    authorize_url = FLOW.step1_get_authorize_url()
+    return HttpResponseRedirect(authorize_url)
 
 def auth_return(request):
     # Oauth callback end point.
